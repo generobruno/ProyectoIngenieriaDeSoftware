120e8020518e9ae58a217fdfcd7d248eab140287
Bruno A. Genero <generobruno@gmail.com>
2022-05-15 14:44:29 -0300

    Método pantalla creado

---
+++ tests/srcTests/graficos/PantallaTest.java
@@ -0,0 +1,16 @@
+package graficos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PantallaTest {
+
+    @Test
+    void limpiar() {
+    }
+
+    @Test
+    void mostrar() {
+    }
+}
