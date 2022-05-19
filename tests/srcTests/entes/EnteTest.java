5ba99059fbb65657f2b8cd60326bba8607d19fb7
Bruno A. Genero <generobruno@gmail.com>
2022-05-19 14:00:44 -0300

    Tests agregados

---
+++ tests/srcTests/entes/EnteTest.java
@@ -0,0 +1,40 @@
+package entes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnteTest {
+
+    @Test
+    void actualizar() {
+    }
+
+    @Test
+    void mostrar() {
+    }
+
+    @Test
+    void eliminar() {
+    }
+
+    @Test
+    void getPosicionX() {
+    }
+
+    @Test
+    void getPosicionY() {
+    }
+
+    @Test
+    void isEliminado() {
+    }
+
+    @Test
+    void modificarPosicionX() {
+    }
+
+    @Test
+    void modificarPosicionY() {
+    }
+}
---
+++ tests/srcTests/entes/criaturas/JugadorTest.java
@@ -0,0 +1,16 @@
+package entes.criaturas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JugadorTest {
+
+    @Test
+    void actualizar() {
+    }
+
+    @Test
+    void mostrar() {
+    }
+}
