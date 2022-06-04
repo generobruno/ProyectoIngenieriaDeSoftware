dbab5d8b6839e993e17f05779a8c5aa81c4302b9
Bruno A. Genero <generobruno@gmail.com>
2022-06-04 16:27:59 -0300

    Clases para Enemigo creadas

---
+++ src/main/java/entes/criaturas/Zombie.java
@@ -0,0 +1,8 @@
+package entes.criaturas;
+
+public class Zombie extends Enemigo {
+
+    public Zombie(int idEnemigo, String nombre, int vidaMax) {
+        super(idEnemigo, nombre, vidaMax);
+    }
+}
